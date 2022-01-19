b28489e41dddd3fbc3399b59a9451e2fc487721e
bishab <bishabpokharel503@gmail.com>
2022-01-19 09:19:10 +0545

    important changes made

--- actions/actions.py
+++ actions/actions.py
@@ -25,3 +25,38 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+
+
+
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+class ActionHelloWorld(Action):
+    def name(self) -> Text:
+        return "from_action_file"
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(text="This is from action file!")
+        return []
+
+class ActionRememberName(Action):
+    def name(self) -> Text:
+        return "utter_remember_name"
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(text=f"Okay I will remember your name {text}")
+        return [SlotSet("name",text)]
+
+class ActionSayName(Action):
+    def name(self) -> Text:
+        return "utter_say_name"
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(text=f"Your name is {tracker.get_slot('name')}")
+        return []
+
