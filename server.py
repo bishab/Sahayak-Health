d34139c7d65153458ed066bc66f1bee34aac812e
bishab <bishabpokharel503@gmail.com>
2022-01-21 21:11:52 +0545

    server updated

--- server.py
+++ server.py
@@ -2,6 +2,16 @@
 import http.server # Our http server handler for http requests
 import socketserver # Establish the TCP Socket connections
  
+
+if '*' in self.cors_origins:
+    request.setHeader('Access-Control-Allow-Origin', '*')
+    request.setHeader('Access-Control-Allow-Headers', 'Content-Type')  # ADDED
+    request.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')  # ADDED
+elif origin in self.cors_origins:
+    request.setHeader('Access-Control-Allow-Origin', origin)
+    request.setHeader('Access-Control-Allow-Headers', 'Content-Type')  # ADDED
+    request.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')  # ADDED
+
 PORT = 8000
  
 class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
