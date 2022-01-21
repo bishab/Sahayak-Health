f795bb4f5e508dd36c337d13b95eb702873fe8db
bishab <bishabpokharel503@gmail.com>
2022-01-21 11:16:44 +0545

    rasa deployment updated

---
+++ server.py
@@ -0,0 +1,17 @@
+# server.py
+import http.server # Our http server handler for http requests
+import socketserver # Establish the TCP Socket connections
+ 
+PORT = 9000
+ 
+class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path=='/':
+            self.path = 'index.html'
+        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+ 
+Handler = MyHttpRequestHandler
+ 
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("Http Server Serving at port", PORT)
+    httpd.serve_forever()
