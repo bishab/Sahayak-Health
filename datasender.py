4b246d1ba8a6ac6b95b1d5444fe78ab6e68d6642
bishab <bishabpokharel503@gmail.com>
2022-02-10 15:19:19 +0545

    db_app added

---
+++ datasender.py
@@ -0,0 +1,2 @@
+import requests
+requests.post(url="http://localhost:8000/getreg/", data={"first_name":"hari","last_name":"lal","email":"ram23232@gmail.com","password":"1212","contact_number":"455454"})
