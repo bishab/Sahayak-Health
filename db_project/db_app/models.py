bc775d426ac07d32897b6050eaff22cced92a005
bishab <bishabpokharel503@gmail.com>
2022-02-10 16:38:39 +0545

    made email unique

--- db_project/db_app/models.py
+++ db_project/db_app/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class RegistrationModel(models.Model):
     first_name=models.CharField(max_length=20)
     last_name=models.CharField(max_length=20)
-    email=models.EmailField(max_length=50)
+    email=models.EmailField(max_length=50,unique=True)
     password=models.CharField(max_length=20)
-    contact_number=models.CharField(max_length=13)
+    contact_number=models.CharField(max_length=13,unique=True)
 
     def __str__(self):
         return self.first_name +" "+ self. last_name
