3eab58efae101e3bb1743c0bc5bf8d87a348061b
bishab <bishabpokharel503@gmail.com>
2022-02-13 21:37:40 +0545

    chatbot integrated by bigyan and bishab is pushing on behalf of bigyan

--- src/components/Chatbot.js
+++ src/components/Chatbot.js
@@ -2,35 +2,37 @@ import React, { useState, useEffect } from "react";
 
 import { Widget, addResponseMessage, setQuickButtons } from "react-chat-widget";
 import "react-chat-widget/lib/styles.css";
-import "./Chatbot.css";
-function Chatbot() {
-  // const buttons = [
-  //   { label: "Call your nearest Hospital", value: "1" },
-  //   { label: "Seek Consultation", value: "2" },
-  // ];
-
+import axios from "axios";
+const Chatbot = () => {
   useEffect(() => {
     addResponseMessage("Ask us anything about COVID-19 ");
     //
-
-    // setQuickButtons(buttons);
   }, []);
 
-  const handleNewUserMessage = (newMessage) => {
+  // const [data, setData] = useState([]);
+  const handleNewUserMessage = async (newMessage) => {
     console.log(`New message incoming! ${newMessage}`);
     // Now send the message throught the backend API
-    let response = "I am getting your msg";
-    addResponseMessage(response);
+    const message = {
+      message: `${newMessage}`,
+    };
+    const res = await axios.post(
+      "http://13.71.80.137:5005/webhooks/rest/webhook",
+      message
+    );
+    addResponseMessage(res.data[0].text);
   };
+
   return (
     <div className="App">
       <Widget
         title={"Bigyan's covid help line"}
-        subtitle={"Wassap Brothers"}
+        subtitle={"Wassap bros"}
         handleNewUserMessage={(message) => handleNewUserMessage(message)}
         emojis="yes"
       />
     </div>
   );
-}
+};
+
 export default Chatbot;
