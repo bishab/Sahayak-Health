bb0f895d821c0063820ca1a1d9315a36a3fe9aa3
bishab <bishabpokharel503@gmail.com>
2022-02-16 14:14:56 +0545

    settings.py updated

--- db_project/db_project/settings.py
+++ db_project/db_project/settings.py
@@ -131,5 +131,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'elvisforyou45@gmail.com'
-EMAIL_HOST_PASSWORD = 'Zorg13007abcd'
+EMAIL_HOST_USER = 'emailsenderai035@gmail.com'
+EMAIL_HOST_PASSWORD = 'CAPSTONEKRGC0137'
