4945671938dac0880f7dd6c3a6e80dad476c9b19
bishab <bishabpokharel503@gmail.com>
2022-03-02 08:10:26 +0545

    Appointment model error fixed

--- db_project/db_app/models.py
+++ db_project/db_app/models.py
@@ -23,7 +23,7 @@ class AppointmentModel(models.Model):
     time=models.CharField(max_length=50)
 
     def __str__(self):
-        return self.hospital +" "+ self.patient+" "+self.date
+        return self.hospital +" "+ self.patient_email+" "+self.date
 
 
 
