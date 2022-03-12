93aa9175f417169f13b9e9c26b460137baa35ebb
GaneshTimilsina <ganesh.timilsina777@gmail.com>
2022-03-13 00:28:25 +0545

    Navigation_Drawer Created

--- app/src/main/java/com/sabkokura/sahayekhelath/MainActivity.java
+++ app/src/main/java/com/sabkokura/sahayekhelath/MainActivity.java
@@ -1,14 +1,84 @@
 package com.sabkokura.sahayekhelath;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+import com.sabkokura.sahayekhelath.Fragments.FragmentAppointment;
+import com.sabkokura.sahayekhelath.Fragments.FragmentFaq;
+import com.sabkokura.sahayekhelath.Fragments.FragmentHome;
+import com.sabkokura.sahayekhelath.Fragments.FragmentHospital;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //defining views
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawlayout);
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.navitgation_open, R.string.navitgation_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        if (savedInstanceState ==null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentHome()).commit();
+            navigationView.setCheckedItem(R.id.nav_menu_home);
+        }
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_menu_home:{
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentHome()).commit();
+                break;
+            }
+            case R.id.nav_menu_faq:{
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentFaq()).commit();
+                break;
+            }
+            case R.id.nav_menu_hospitalList:{
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentHospital()).commit();
+                break;
+            }
+            case R.id.nav_menu_appointment:{
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentAppointment()).commit();
+                break;
+            }
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+
+        return true;
     }
 }
